012698fc56d7311651b02aec9aac8aae6593f38f
fanguangcheng <Fansu1990>
2021-09-27 20:23:43 +0800

    移除 Android zxing-android-embedded库，主要是二维码模糊的情况下扫描效果一般

--- app/src/main/java/com/example/guangchengfan/myview/qrcode/QrCodeActivity.kt
+++ app/src/main/java/com/example/guangchengfan/myview/qrcode/QrCodeActivity.kt
@@ -4,16 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.guangchengfan.myview.R
-import com.google.zxing.integration.android.IntentIntegrator
-import android.widget.Toast
-
-import com.google.zxing.integration.android.IntentResult
-
 import android.content.Intent
-import android.net.Uri
-import com.google.zxing.qrcode.QRCodeReader
-
-
 /**
  * @author :fgc
  * @date :2021/9/27
@@ -25,25 +16,12 @@ class QrCodeActivity : AppCompatActivity() {
     }
 
     fun onQrBtnClick(view : View) {
-        IntentIntegrator(this)
-            .setPrompt("请对准二维码1")// 设置提示语
-            .initiateScan();// 初始化扫码
-        QrCodeScanUtil.scanImg(this)
     }
 
 
 
     // Get the results:
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-        if (result != null) {
-            if (result.contents == null) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
-            } else {
-                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
-        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
--- app/src/main/java/com/example/guangchengfan/myview/qrcode/QrCodeScanUtil.java
+++ app/src/main/java/com/example/guangchengfan/myview/qrcode/QrCodeScanUtil.java
@@ -3,24 +3,10 @@ package com.example.guangchengfan.myview.qrcode;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.util.Log;
-
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.ChecksumException;
-import com.google.zxing.FormatException;
-import com.google.zxing.NotFoundException;
-import com.google.zxing.RGBLuminanceSource;
-import com.google.zxing.Reader;
-import com.google.zxing.Result;
-import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.QRCodeReader;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 
 /**
  * @author :fgc
@@ -40,7 +26,7 @@ public class QrCodeScanUtil {
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    scanImage(bitmap, context);
+//                    scanImage(bitmap, context);
                 }
             });
             thread.start();
@@ -49,49 +35,4 @@ public class QrCodeScanUtil {
         }
     }
 
-    public static Result scanImage(Bitmap bitmap, Context context) {
-        Result result = null;
-        try {
-//            Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
-//            bitmap = getSmallerBitmap(bitmap);
-            if (bitmap != null) {
-                int width = bitmap.getWidth();
-                int height = bitmap.getHeight();
-                int[] pixels = new int[width * height];
-                bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
-
-                RGBLuminanceSource source = new RGBLuminanceSource(width, height, pixels);
-                BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(source));
-                Reader reader = new QRCodeReader();
-                try {
-                    result = reader.decode(binaryBitmap);
-                } catch (NotFoundException | ChecksumException | FormatException e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-         }
-
-        if (result != null) {
-            Log.d("QrCodeScanUtil", result.getText());
-        } else {
-            Log.d("QrCodeScanUtil", "get result error");
-        }
-        return result;
-    }
-
-    private static Bitmap getSmallerBitmap(Bitmap bitmap) {
-        if (bitmap != null) {
-            int size = bitmap.getWidth() * bitmap.getHeight() / 160000;
-            if (size <= 1) {
-                return bitmap;
-            } else {
-                Matrix matrix = new Matrix();
-                matrix.postScale((float) (1 / Math.sqrt(size)), (float) (1 / Math.sqrt(size)));
-                return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-            }
-        }
-        return null;
-    }
 }
