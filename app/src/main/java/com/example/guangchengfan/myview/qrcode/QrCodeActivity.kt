aaaae2902cb6c1d46e51fec9dc3d088f5baa3908
fanguangcheng <Fansu1990>
2021-09-27 20:52:38 +0800

    引入com.huawei.hms:scanplus   华为二维码扫描库，效果更好，模糊图片容错度更高

--- app/src/main/java/com/example/guangchengfan/myview/qrcode/QrCodeActivity.kt
+++ app/src/main/java/com/example/guangchengfan/myview/qrcode/QrCodeActivity.kt
@@ -5,23 +5,55 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.guangchengfan.myview.R
 import android.content.Intent
+import android.provider.MediaStore
+import android.text.TextUtils
+import com.huawei.hms.hmsscankit.ScanUtil
+import com.huawei.hms.ml.scan.HmsScanAnalyzerOptions
+import java.io.IOException
+
 /**
  * @author :fgc
  * @date :2021/9/27
  */
 class QrCodeActivity : AppCompatActivity() {
+
+    companion object{
+        const val SCAN_RESULT = "scanResult"
+        const val REQUEST_CODE_PHOTO = 0X1113
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qrcode)
     }
 
     fun onQrBtnClick(view : View) {
+//        QrCodeScanUtil.scanImg(this)
+        val pickIntent = Intent(Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        pickIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+        this@QrCodeActivity.startActivityForResult(pickIntent, REQUEST_CODE_PHOTO)
     }
 
-
-
-    // Get the results:
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_PHOTO) {
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, data?.data)
+                QrCodeScanUtil.scanImg(this, bitmap)
+
+//                val hmsScans = ScanUtil.decodeWithBitmap(this@QrCodeActivity, bitmap, HmsScanAnalyzerOptions.Creator().setPhotoMode(true).create())
+//                if (hmsScans != null && hmsScans.size > 0 && hmsScans[0] != null && !TextUtils.isEmpty(hmsScans[0]!!.getOriginalValue())) {
+//                    val intent = Intent()
+//                    intent.putExtra(SCAN_RESULT, hmsScans[0])
+//                    setResult(RESULT_OK, intent)
+//                    finish()
+//                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
+
+
 }
--- app/src/main/java/com/example/guangchengfan/myview/qrcode/QrCodeScanUtil.java
+++ app/src/main/java/com/example/guangchengfan/myview/qrcode/QrCodeScanUtil.java
@@ -5,6 +5,12 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.util.Log;
+
+import com.huawei.hms.hmsscankit.ScanUtil;
+import com.huawei.hms.ml.scan.HmsScan;
+import com.huawei.hms.ml.scan.HmsScanAnalyzerOptions;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
@@ -14,19 +20,23 @@ import java.io.FileNotFoundException;
  */
 public class QrCodeScanUtil {
 
-    public static void scanImg(Context context) {
+    public static void scanImg(Context context, Bitmap bitmap) {
         try {
             FileInputStream fs = null;
-//            fs = new FileInputStream("/sdcard/1/1234.png");
+            fs = new FileInputStream("/sdcard/1/1234.png");
 //            fs = new FileInputStream("/sdcard/1/long.jpg");
-            fs = new FileInputStream("/sdcard/1/short.jpg");
-            Bitmap bitmap  = BitmapFactory.decodeStream(fs);
+//            fs = new FileInputStream("/sdcard/1/short.jpg");
+//            Bitmap bitmap  = BitmapFactory.decodeStream(fs);
 
             Uri imageUri = Uri.parse(MediaStore.Images.Media.insertImage(context.getContentResolver(),bitmap , null,null));
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
 //                    scanImage(bitmap, context);
+                    HmsScan[] result = ScanUtil.decodeWithBitmap(context, bitmap, new HmsScanAnalyzerOptions.Creator().setHmsScanTypes(HmsScan.ALL_SCAN_TYPE).setPhotoMode(true).create());
+                    if (result != null) {
+                        Log.d("QrCodeScanUtil", "result: " + result[0].originalValue);
+                    }
                 }
             });
             thread.start();
@@ -35,4 +45,13 @@ public class QrCodeScanUtil {
         }
     }
 
+//    private fun decodeBitmap(bitmap: Bitmap, scanType: Int) {
+//        val hmsScans = ScanUtil.decodeWithBitmap(this@CommonActivity, bitmap, HmsScanAnalyzerOptions.Creator().setHmsScanTypes(scanType).setPhotoMode(true).create())
+//        if (hmsScans != null && hmsScans.size > 0 && hmsScans[0] != null && !TextUtils.isEmpty(hmsScans[0]!!.getOriginalValue())) {
+//            val intent = Intent()
+//            intent.putExtra(SCAN_RESULT, hmsScans)
+//            setResult(RESULT_OK, intent)
+//            finish()
+//        }
+//    }
 }
